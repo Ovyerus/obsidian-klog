c988b2386d4147eb55979224287b3133cdda85f4
Ovyerus <ovy@ovyerus.com>
2024-04-28 20:13:08 +1000

    chore: update version bump script

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,8 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = process.env.npm_package_version;
+const { version: targetVersion } = JSON.parse(
+  readFileSync("package.json", "utf8"),
+);
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
