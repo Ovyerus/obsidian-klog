6f6a6f25b6019100418d014da69d4ec75ebb3028
Ovyerus <ovy@ovyerus.com>
2024-04-28 23:28:16 +1000

    chore: add CI & building through flake

--- flake.nix
+++ flake.nix
@@ -2,26 +2,39 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    pnpm2nix = {
+      url = "github:nzbr/pnpm2nix-nzbr";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = {
     flake-utils,
     nixpkgs,
+    pnpm2nix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
+          overlays = [pnpm2nix.outputs.overlays.default];
           inherit system;
         };
       in {
         devShells.default = with pkgs;
           mkShell {
             buildInputs = [
-              nodejs_20
-              corepack_20
+              nodejs
+              nodejs.pkgs.pnpm
             ];
           };
+
+        packages.default = pkgs.mkPnpmPackage {
+          src = ./.;
+        };
       }
     );
 }
